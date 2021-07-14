48a2b1f7cae32ecb89eaa421f534192938f903e2
ainch <chldbsdu1001@naver.com>
2021-07-14 22:41:22 +0900

    Add createVideo() for typescript type test

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-console.log("hello");
+const createVideo = (id: number, name?: string): void => {
+    console.log(`Video which id is ${id}, name is ${name} is created.`);
+}
+
+createVideo(1, "LOL");
+createVideo(1);
